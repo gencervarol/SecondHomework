20802f9b6811ff79f3556e5b0752d6606656f669
gencervarol <gencervarol@users.noreply.github.com>
2021-03-10 21:56:16 +0300

    Add project files.

---
+++ SecondHomework/Dictionary.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SecondHomework
+{
+    class Dictionary<PN, FP>
+    {   //PN: Player Number--FP: Football Player
+        PN[] number;
+        FP[] name;
+        public Dictionary()
+        {
+            number = new PN[0];
+            name = new FP[0];
+        }
+        public void Add(PN numbers, FP names)
+        {
+            PN[] tempNo = number;
+            FP[] tempName = name;
+
+            number = new PN[number.Length + 1];
+            name = new FP[name.Length + 1];
+
+            for (int i = 0; i < tempNo.Length; i++)
+            {
+                number[i] = tempNo[i];
+            }
+            number[number.Length - 1] = numbers;
+
+            for (int i = 0; i < tempName.Length; i++)
+            {
+                name[i] = tempName[i];
+            }
+            name[name.Length - 1] = names;
+        }
+
+        public void ToList()
+        {
+            for (int i = 0; i < number.Length; i++)
+            {
+                Console.WriteLine(number[i] + ":" + name[i]);
+            }
+
+
+        }
+    }
+}
---
+++ SecondHomework/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace SecondHomework
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<int, string> team = new Dictionary<int, string>();
+
+            team.Add(20, "Gustavo");
+            team.Add(27, "Mame Thiam");
+            team.Add(41, "Attila Szalai");
+            team.Add(8, "Mert Hakan Yandaş");
+            team.Add(7, "Ozan Tufan");
+            team.Add(5, "Sosa");
+
+
+            team.ToList();
+
+
+
+        }
+    }
+}
